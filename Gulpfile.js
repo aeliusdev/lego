9b9f2885c1b33b3b3fd1162cdd4e4d4a9b69ca27
System Administrator <root@Katies-iMac.local>
2016-04-08 14:50:46 +0100

    Added SCSS, Few DIVS in index.html

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,2 +1,12 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+
+gulp.task('comp_sass', function () {
+  return gulp.src('./assets/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./assets/css/'));
+});
+ 
+gulp.task('default', function () {
+  gulp.watch('./assets/scss/*.scss', ['comp_sass']);
+});
