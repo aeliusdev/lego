3d36a47b6f8feb16836b14994e067ee51628b4fd
System Administrator <root@Katies-iMac.local>
2016-04-08 18:16:39 +0100

    Added slider functionality.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,38 @@
-$(function() {
-    $( "#slider" ).slider();
+$(function() 
+{
+	var el = $("#amount");
+	
+	$( "#calculator-slider-div" ).slider({range: "min",
+		min: 0,
+		max: 10000,
+		value: 2000,
+		slide: function( event, ui ) 
+		{
+			var amount = ui.value;
+
+			if (amount <= 3000)
+			{
+				el.html("3");
+			} else if (amount > 3000 && amount <= 6000)
+			{
+				el.html("2");				
+			} else 
+			{
+				el.html("1");				
+			}
+		}
+	});
+	
+	var amount = $( "#calculator-slider-div" ).slider( "value" );
+	
+	if (amount <= 3000)
+	{
+		el.html("3");
+	} else if (amount > 3000 && amount <= 6000)
+	{
+		el.html("2");				
+	} else 
+	{
+		el.html("1");				
+	}	
 });
