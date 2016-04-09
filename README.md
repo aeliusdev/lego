ef8f1804ba032ecbf0fea0cb4569ebbfb719f947
George <aeliusdev@users.noreply.github.com>
2016-04-09 18:19:12 +0100

    Added Mobile Menu Functionality

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ A trial project.
 RESET CSS - http://meyerweb.com/eric/tools/css/reset/
 JQUERY UI - http://jqueryui.com/download/ 
 JQUERY - http://jquery.com/download/ 
+
+2h34m59s
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,40 +2,51 @@ $(function()
 {
 	var el = $("#amount");
 	var menu_open = false;
+	var initial_amount = 1234;
 	
+	var tooltip = function(event, ui) 
+	{
+		//console.log(ui.value);
+	    var amount = ui.value || initial_amount;
+	    var tooltip = '<div class="tooltip"><div class="tooltip-inner">' + amount + '</div><div class="tooltip-arrow"></div></div>';
+		if (amount <= 3000)
+		{
+			el.html("3");
+		} else if (amount > 3000 && amount <= 6000)
+		{
+			el.html("2");				
+		} else 
+		{
+			el.html("1");				
+		}
+			
+	    $('.ui-slider-handle').html(tooltip);
+	}
+		
 	$( "#calculator-slider-div" ).slider({range: "min",
 		min: 0,
 		max: 10000,
-		value: 2000,
-		slide: function( event, ui ) 
-		{
-			var amount = ui.value;
-
-			if (amount <= 3000)
-			{
-				el.html("3");
-			} else if (amount > 3000 && amount <= 6000)
-			{
-				el.html("2");				
-			} else 
-			{
-				el.html("1");				
-			}
-		}
+		value: initial_amount,
+		create: tooltip,
+		slide: tooltip
 	});
 	
-	var amount = $( "#calculator-slider-div" ).slider( "value" );
-	
-	if (amount <= 3000)
+	$("#mobile-menu-toggle").on("click", function () 
 	{
-		el.html("3");
-	} else if (amount > 3000 && amount <= 6000)
-	{
-		el.html("2");				
-	} else 
-	{
-		el.html("1");				
-	}	
+		if (menu_open == true)
+		{
+			$("#mobile-menu ul").slideUp();
+			$(this).find("span").text("+");
+			
+			menu_open = false;
+		} else
+		{
+			$("#mobile-menu ul").slideUp(0).slideDown(400);
+			$(this).find("span").text("-");
+
+			menu_open = true;
+		}
+	});
 	
 	$(window).resize(function () 
 	{
